82c46f59d49f9a32bff811bb28a4d333d0b6b3b1
Lucas Sierota Silva <lucas.silva@codengage.com>
2021-11-23 16:14:59 -0300

    feat: add base components for UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,186 +1,133 @@
-import { useRouter } from 'next/router';
+import { useCallback, useEffect } from 'react';
 
-import { Meta } from '../layout/Meta';
-import { Main } from '../templates/Main';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { ethers } from 'ethers';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { AppContainer, Container } from '../components/Container';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { CONTRACT_ADDRESS } from '../constants';
+import {
+  SET_CHARACTER_NFT,
+  SET_CURRENT_ACCOUNT,
+  SET_ERROR,
+} from '../context/actions';
+import useGlobalContext from '../hooks/useGlobalContext';
+import Game from '../utils/Game.json';
+import {
+  CharacterDataABI,
+  transformCharacterData,
+} from '../utils/transformCharacterData';
 
 const Index = () => {
-  const router = useRouter();
+  const {
+    state: { currentAccount },
+    dispatch,
+  } = useGlobalContext();
+
+  const { t } = useTranslation('common');
+
+  const checkIfWalletIsConnected = useCallback(async () => {
+    try {
+      const ethereum: any = await detectEthereumProvider();
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      }
+      console.log('We have the ethereum object', ethereum);
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      /*
+       * User can have multiple authorized accounts, we grab the first one if its there!
+       */
+      if (accounts.length !== 0) {
+        const account: string = accounts[0];
+        console.log('Found an authorized account:', account);
+        dispatch({ type: SET_CURRENT_ACCOUNT, payload: account });
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch (error) {
+      dispatch({ type: SET_ERROR, payload: error as string });
+      console.log(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [checkIfWalletIsConnected]);
+
+  useEffect(() => {
+    /*
+     * The function we will call that interacts with out smart contract
+     */
+    const fetchNFTMetadata = async () => {
+      console.log('Checking for Character NFT on address:', currentAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        Game.abi,
+        signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      if (txn.name) {
+        console.log('User has character NFT');
+        const payload = transformCharacterData(txn as CharacterDataABI);
+        dispatch({
+          type: SET_CHARACTER_NFT,
+          payload,
+        });
+      } else {
+        dispatch({ type: SET_ERROR, payload: 'No character NFT found' });
+        console.log('No character NFT found');
+      }
+    };
+
+    /*
+     * We only want to run this, if we have a connected wallet
+     */
+    if (currentAccount) {
+      console.log('CurrentAccount:', currentAccount);
+      fetchNFTMetadata();
+    }
+  }, [currentAccount, dispatch]);
 
   return (
-    <Main
-      meta={
-        <Meta
-          title="Next.js Boilerplate Presentation"
-          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+    <AppContainer>
+      <Container>
+        <Header
+          title={t('emojiName')}
+          subtitle={t('subtitle')}
+          btnText={t('connectWallet')}
+          selectCharacterText={t('selectCharacterText')}
         />
-      }
-    >
-      <a href="https://github.com/ixartz/Next-js-Boilerplate">
-        <img
-          src={`${router.basePath}/assets/images/nextjs-starter-banner.png`}
-          alt="Nextjs starter banner"
-        />
-      </a>
-      <h1 className="font-bold text-2xl">
-        Boilerplate code for your Nextjs project with Tailwind CSS
-      </h1>
-      <p>
-        <span role="img" aria-label="rocket">
-          🚀
-        </span>{' '}
-        Next.js Boilerplate is a starter code for your Next js project by
-        putting developer experience first .{' '}
-        <span role="img" aria-label="zap">
-          ⚡️
-        </span>{' '}
-        Made with Next.js, TypeScript, ESLint, Prettier, Husky, Lint-Staged,
-        VSCode, Netlify, PostCSS, Tailwind CSS.
-      </p>
-      <h2 className="font-semibold text-lg">Next js Boilerplate Features</h2>
-      <p>Developer experience first:</p>
-      <ul>
-        <li>
-          <span role="img" aria-label="fire">
-            🔥
-          </span>{' '}
-          <a href="https://nextjs.org" rel="nofollow">
-            Next.js
-          </a>{' '}
-          for Static Site Generator
-        </li>
-        <li>
-          <span role="img" aria-label="art">
-            🎨
-          </span>{' '}
-          Integrate with{' '}
-          <a href="https://tailwindcss.com" rel="nofollow">
-            Tailwind CSS
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="nail_care">
-            💅
-          </span>{' '}
-          PostCSS for processing Tailwind CSS
-        </li>
-        <li>
-          <span role="img" aria-label="tada">
-            🎉
-          </span>{' '}
-          Type checking Typescript
-        </li>
-        <li>
-          <span role="img" aria-label="pencil2">
-            ✏️
-          </span>{' '}
-          Linter with{' '}
-          <a href="https://eslint.org" rel="nofollow">
-            ESLint
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="hammer_and_wrench">
-            🛠
-          </span>{' '}
-          Code Formatter with{' '}
-          <a href="https://prettier.io" rel="nofollow">
-            Prettier
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="fox_face">
-            🦊
-          </span>{' '}
-          Husky for Git Hooks
-        </li>
-        <li>
-          <span role="img" aria-label="no_entry_sign">
-            🚫
-          </span>{' '}
-          Lint-staged for running linters on Git staged files
-        </li>
-        <li>
-          <span role="img" aria-label="no_entry_sign">
-            🗂
-          </span>{' '}
-          VSCode configuration: Debug, Settings, Tasks and extension for
-          PostCSS, ESLint, Prettier, TypeScript
-        </li>
-        <li>
-          <span role="img" aria-label="robot">
-            🤖
-          </span>{' '}
-          SEO metadata, JSON-LD and Open Graph tags with Next SEO
-        </li>
-        <li>
-          <span role="img" aria-label="robot">
-            ⚙️
-          </span>{' '}
-          <a
-            href="https://www.npmjs.com/package/@next/bundle-analyzer"
-            rel="nofollow"
-          >
-            Bundler Analyzer
-          </a>
-        </li>
-        <li>
-          <span role="img" aria-label="rainbow">
-            🌈
-          </span>{' '}
-          Include a FREE minimalist theme
-        </li>
-        <li>
-          <span role="img" aria-label="hundred">
-            💯
-          </span>{' '}
-          Maximize lighthouse score
-        </li>
-      </ul>
-      <p>Built-in feature from Next.js:</p>
-      <ul>
-        <li>
-          <span role="img" aria-label="coffee">
-            ☕
-          </span>{' '}
-          Minify HTML &amp; CSS
-        </li>
-        <li>
-          <span role="img" aria-label="dash">
-            💨
-          </span>{' '}
-          Live reload
-        </li>
-        <li>
-          <span role="img" aria-label="white_check_mark">
-            ✅
-          </span>{' '}
-          Cache busting
-        </li>
-      </ul>
-      <h2 className="font-semibold text-lg">Our Stater code Philosophy</h2>
-      <ul>
-        <li>Minimal code</li>
-        <li>SEO-friendly</li>
-        <li>
-          <span role="img" aria-label="rocket">
-            🚀
-          </span>{' '}
-          Production-ready
-        </li>
-      </ul>
-      <p>
-        Check our GitHub project for more information about{' '}
-        <a href="https://github.com/ixartz/Next-js-Boilerplate">
-          Nextjs Boilerplate
-        </a>
-        . You can also browse our{' '}
-        <a href="https://creativedesignsguru.com/category/nextjs/">
-          Premium NextJS Templates
-        </a>{' '}
-        on our website to support this project.
-      </p>
-    </Main>
+        <Footer />
+      </Container>
+    </AppContainer>
   );
 };
 
 export default Index;
+
+interface GetStaticPropsProps {
+  locale: string;
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsProps) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'footer'])),
+      // Will be passed to the page component as props
+    },
+  };
+}
